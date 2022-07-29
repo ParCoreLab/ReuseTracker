fb164ab62f88a72b296400afd3d818eb491b1be4
msasongko17 <msasongko17@ku.edu.tr>
2022-07-29 09:48:19 +0300

    parsec_l1_l3_basarim

---
+++ parsec_experiment/parsec_l1_l3_basarim.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+./blackscholes_l1_run_cacheline_inv_gold.sh $1
+./blackscholes_l1_run_cacheline_inv_gold_spatial.sh $1
+./bodytrack_l1_run_cacheline_inv_gold.sh $1
+./bodytrack_l1_run_cacheline_inv_gold_spatial.sh $1
+./streamcluster_l1_run_cacheline_inv_gold.sh $1
+./streamcluster_l1_run_cacheline_inv_gold_spatial.sh $1
+./freqmine_l1_run_cacheline_inv_gold.sh $1
+./freqmine_l1_run_cacheline_inv_gold_spatial.sh $1
+./blackscholes_l3_run_cacheline_inv_gold.sh $1
+./blackscholes_l3_run_cacheline_inv_gold_spatial.sh $1
+./bodytrack_l3_run_cacheline_inv_gold.sh $1
+./bodytrack_l3_run_cacheline_inv_gold_spatial.sh $1
+./streamcluster_l3_run_cacheline_inv_gold.sh $1
+./streamcluster_l3_run_cacheline_inv_gold_spatial.sh $1
+./freqmine_l3_run_cacheline_inv_gold.sh $1
+./freqmine_l3_run_cacheline_inv_gold_spatial.sh $1
